161a7a744f9d821c1b77135ae6c8b7df79558730
Antonio1H <100202593+Antonio1H@users.noreply.github.com>
2024-02-19 20:34:44 -0300

    No.java

---
+++ No.java
@@ -0,0 +1,11 @@
+public class No{
+    public Integer key;
+    public No left;
+    public No right;
+
+    public No(Integer key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
